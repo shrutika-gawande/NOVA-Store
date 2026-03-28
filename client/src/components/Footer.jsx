8999d3c515df50baed7e1e15287cc3360a72f6be
shrutika-gawande <shrutikagawande80@gmail.com>
2026-03-28 18:05:26 +0530

    added footer and its styling

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../styles/footer.css"
+
+function Footer() {
+  return (
+    <footer>
+        <p>© 2025 NOVA Store — Built with ♥ for the internship task &nbsp;|&nbsp; <a href="#">GitHub</a> &nbsp;|&nbsp; <a href="#">Deploy</a></p>
+    </footer>
+  )
+}
+
+export default Footer
