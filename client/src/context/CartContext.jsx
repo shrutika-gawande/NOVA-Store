26f7377cc21cf964bda005e1f88952c62dda2e8e
shrutika-gawande <shrutikagawande80@gmail.com>
2026-04-13 23:00:22 +0530

    implemented cartContext

---
+++ client/src/context/CartContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useReducer } from 'react';
+
+const CartContext = createContext();
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD': {
+      const exists = state.items.find(i => i.id === action.product.id);
+      if (exists) {
+        return {
+          ...state,
+          items: state.items.map(i =>
+            i.id === action.product.id ? { ...i, qty: i.qty + 1 } : i
+          ),
+        };
+      }
+      return { ...state, items: [...state.items, { ...action.product, qty: 1 }] };
+    }
+    case 'REMOVE':
+      return { ...state, items: state.items.filter(i => i.id !== action.id) };
+    case 'CLEAR':
+      return { ...state, items: [] };
+    default:
+      return state;
+  }
+};
+
+export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, { items: [] });
+  const totalItems = state.items.reduce((s, i) => s + i.qty, 0);
+  const totalPrice = state.items.reduce((s, i) => s + i.price * i.qty, 0);
+  return (
+    <CartContext.Provider value={{ ...state, totalItems, totalPrice, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
+ 
