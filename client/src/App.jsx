da9ceea14d092f0ddb14a3bc9e4e671f14b1846c
shrutika-gawande <shrutikagawande80@gmail.com>
2026-04-14 21:54:23 +0530

    wrapped with BrowserRouter and CartProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
+import { Toaster } from "react-hot-toast";
+import { CartProvider } from "./context/CartContext";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import HomePage from "./pages/HomePage"
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
-  )
+    <BrowserRouter>
+
+      <CartProvider>
+        <Routes>
+          
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="*" element={<Navigate to="/" replace />} />
+
+        </Routes>
+        <Toaster position="bottom-right" />
+      </CartProvider>
+    </BrowserRouter>
+
+  );
 }
 
 export default App
