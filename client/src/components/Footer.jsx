77bfbd4bdecf008d0feeb06f754fa12c6d69915e
shrutika-gawande <shrutikagawande80@gmail.com>
2026-04-17 22:04:21 +0530

    added github link

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../styles/footer.css"
 function Footer() {
   return (
     <footer>
-        <p>© {new Date().getFullYear()} NOVA Store — Enjoy Shopping ♥  &nbsp;|&nbsp; <a href="#">GitHub</a> &nbsp;|&nbsp; <a href="#">Deploy</a></p>
+        <p>© {new Date().getFullYear()} NOVA Store — Enjoy Shopping ♥  &nbsp;|&nbsp; <a href="https://github.com/shrutika-gawande/NOVA-Store">GitHub</a> &nbsp;|&nbsp; <a href="#">Deploy</a></p>
     </footer>
   )
 }
